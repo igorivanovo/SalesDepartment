f789777ce6b558c31f179f189b147783d2766b3f
Igor Arhipov <igor.arhipov.69@mail.ru>
2023-02-15 17:21:47 +0300

    Added min() and average() methogs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {10,45,87,56,90,23,100,23,110,80};
+        int[] sales = {10, 45, 87, 56, 90, 23, 100, 23, 110, 80};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println(" МАКСИМАЛЬНАЯ ПРОДАЖА  "+ salesManager.max() + " РУБ.");
+        System.out.println(" МАКСИМАЛЬНАЯ ПРОДАЖА  " + salesManager.max() + " РУБ.");
+        System.out.println(" МИНИМАЛЬНАЯ  " + salesManager.min());
+        System.out.println("Усредненная продажа  " + salesManager.average());
     }
 }
